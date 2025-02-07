62ff3cd094d9346f13c6728f67c7d2c7ab8b6f18
Chidozie <chidough14@gmail.com>
2025-02-07 14:46:56 +0100

    create categories/departments table with relationships and seeders

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = [
+            [
+                'name' => 'Electronics',
+                'department_id' => 1,
+                'parent_id' => null,
+                'active' => true,
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'name' => 'Fashion',
+                'department_id' => 2,
+                'parent_id' => null,
+                'active' => true,
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            // Sub categories of Electronics
+            [
+                'name' => 'Computers',
+                'department_id' => 1,
+                'parent_id' => 1,
+                'active' => true,
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'name' => 'Smartphones',
+                'department_id' => 1,
+                'parent_id' => 1,
+                'active' => true,
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            // Sub Category of Computers
+            [
+                'name' => 'Laptops',
+                'department_id' => 1,
+                'parent_id' => 3,
+                'active' => true,
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'name' => 'Desktops',
+                'department_id' => 1,
+                'parent_id' => 3,
+                'active' => true,
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            // Sub Category of Smartphones
+            [
+                'name' => 'Android',
+                'department_id' => 1,
+                'parent_id' => 4,
+                'active' => true,
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'name' => 'IPhones',
+                'department_id' => 1,
+                'parent_id' => 4,
+                'active' => true,
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+        ];
+
+        DB::table('categories')->insert($categories);
+    }
+}
