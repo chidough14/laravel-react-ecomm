eee4b2bee00f0516d7b1ae1ffa0cf27ef82393f5
Chidozie <chidough14@gmail.com>
2025-02-10 10:30:43 +0100

    implement image uploading on products

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -7,6 +7,7 @@ use App\Enums\RolesEnum;
 use App\Filament\Resources\ProductResource\Pages;
 use App\Filament\Resources\ProductResource\Pages\EditProduct;
 use App\Filament\Resources\ProductResource\Pages\ProductImages;
+use App\Filament\Resources\ProductResource\Pages\ProductVariationTypes;
 use App\Filament\Resources\ProductResource\RelationManagers;
 use App\Models\Product;
 use Filament\Facades\Filament;
@@ -151,6 +152,7 @@ class ProductResource extends Resource
             'create' => Pages\CreateProduct::route('/create'),
             'edit' => Pages\EditProduct::route('/{record}/edit'),
             'images' => Pages\ProductImages::route('/{record}/images'),
+            'variation-types' => Pages\ProductVariationTypes::route('/{record}/variation-types'),
         ];
     }
 
@@ -158,7 +160,8 @@ class ProductResource extends Resource
     {
         return $page->generateNavigationItems([
                 EditProduct::class,
-                ProductImages::class
+                ProductImages::class,
+                ProductVariationTypes::class
         ]);
         
     }
